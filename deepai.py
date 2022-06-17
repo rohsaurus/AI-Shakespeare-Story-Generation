c8bfbd6ce8dec68c8763f00e364618803606b36a
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2022-06-17 16:56:58 +0000

    cursed TTS

--- deepai.py
+++ deepai.py
@@ -1,14 +1,19 @@
 import requests
+from text_to_speech import speak
+#speak("Dick and Jake's", "en", save=True, file="Hello-World.mp3", speak=True)
 
-userInput = input("Enter some starter text here for the model to generate the rest of the story\n")
+while True:
+    userInput = input("Enter some starter text here for the model to generate the rest of the story\n")
 
 
-r = requests.post(
-    "https://api.deepai.org/api/text-generator",
-    data={
-        'text': userInput,
-    },
-    headers={'api-key': 'fc29a633-b891-4204-8db0-ac87cd81226b'}
-)
-print("\n\n\n\n")
-print(r.json()['output'])
+    r = requests.post(
+        "https://api.deepai.org/api/text-generator",
+        data={
+            'text': userInput,
+        },
+        headers={'api-key': 'fc29a633-b891-4204-8db0-ac87cd81226b'}
+    )
+    print("\n\n\n\n")
+    print(r.json()['output'])
+    #speak(r.json()['output'], "en", save=True, file="Hello-World.mp3", speak=True)
+    print("\n\n\n\n")
